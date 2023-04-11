1c5abed538cfc0158aca87ca17df174bfc6bd4be
Fethi <fethiguneyy@gmail.com>
2023-04-11 12:34:32 +0300

    11/04

--- src/bıss/Question3.java
+++ src/bıss/Question3.java
@@ -3,7 +3,7 @@ package bıss;
 public class Question3 {
 
     public static void main(String[] args) {
-        int[] input = {3, 34, 4, 12, 5, 2};
+        int[] input = {-3, 34, 4, 12, 5, 2};
         int sum = 9;
 
         if (isSubsetSum(input, sum)) {
@@ -18,22 +18,32 @@ public class Question3 {
 
 
     public static boolean isSubsetSum(int[] input, int sum) {
+
         int n = input.length;
         boolean[][] subset = new boolean[sum + 1][n + 1];
-        for (int i = 0; i <= n; i++) {
-            subset[0][i] = true;
-        }
-        for (int i = 1; i <= sum; i++) {
-            subset[i][0] = false;
-        }
-        for (int i = 1; i <= sum; i++) {
-            for (int j = 1; j <= n; j++) {
-                subset[i][j] = subset[i][j - 1];
-                if (i >= input[j - 1]) {
-                    subset[i][j] = subset[i][j] || subset[i - input[j - 1]][j - 1];
+        for (int k = 0; k < input.length ; k++) {
+            if(input [k] < 0){
+                System.out.println("All inputs must be non-negatif integer");
+
+            } else {
+                for (int i = 0; i <= n; i++) {
+                    subset[0][i] = true;
+                }
+                for (int i = 1; i <= sum; i++) {
+                    subset[i][0] = false;
+                }
+                for (int i = 1; i <= sum; i++) {
+                    for (int j = 1; j <= n; j++) {
+                        subset[i][j] = subset[i][j - 1];
+                        if (i >= input[j - 1]) {
+                            subset[i][j] = subset[i][j] || subset[i - input[j - 1]][j - 1];
+                        }
+                    }
                 }
             }
         }
+
+
 
         return subset[sum][n];
     }
